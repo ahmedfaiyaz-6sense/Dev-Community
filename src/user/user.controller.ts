2fb60fc09324a28e6f0c05575b9984e23eed192a
fuzzy <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2024-11-27 13:14:48 +0600

    added basic auth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,11 +2,17 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDTO } from './dto/user.createuser.dto';
 import { User } from './user.schema';
 import { UserService } from './user.service';
+import { LoginUserDTO } from './dto/user.loginuser.dto';
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
-  @Post()
+  @Post('/signup')
   createUser(@Body() user: CreateUserDTO): Promise<User> {
     return this.userService.createUser(user);
+  }
+
+  @Post('/login')
+  loginUser(@Body() user: LoginUserDTO): Promise<User> {
+    return this.userService.loginUser(user);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateUserDTO } from './dto/user.createuser.dto';
 import { Model } from 'mongoose';
 import { User } from './user.schema';
 import { InjectModel } from '@nestjs/mongoose';
+import * as bcrypt from 'bcrypt';
+import { LoginUserDTO } from './dto/user.loginuser.dto';
 @Injectable()
 export class UserService {
   constructor(
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ) {}
+
   public async createUser(user: CreateUserDTO): Promise<User> {
-    const createdUser = await this.userModel.create(user);
-    return createdUser;
+    const salt = await bcrypt.genSalt();
+    const hashed_password = await bcrypt.hash(user.password, salt);
+    user.password = hashed_password;
+    try {
+      const createdUser = await this.userModel.create(user);
+      return createdUser;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new ConflictException('Username already exists.');
+      } else {
+        throw new InternalServerErrorException('Unknown error function');
+      }
+    }
+  }
+  public async loginUser(user: LoginUserDTO): Promise<User> {
+    const found_user = await this.userModel.find({ username: user.username });
+    if (!found_user.length) {
+      throw new NotFoundException('Username or password is wrong');
+    }
+    const verify = bcrypt.compare(found_user[0].password, user.password);
+    if (!verify) {
+      throw new NotFoundException('Username or password is wrong');
+    } else {
+      return found_user[0];
+    }
   }
 }
