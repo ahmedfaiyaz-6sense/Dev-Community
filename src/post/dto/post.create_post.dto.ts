5b1e48362e130e09472ae1931ebdb8b182708261
fuzzy <ahmed.faiyaz1532@gmail.com>
2024-11-27 18:20:35 +0600

    added swagger api docs

--- src/post/dto/post.create_post.dto.ts
+++ src/post/dto/post.create_post.dto.ts
@@ -1,10 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 import { User } from 'src/user/user.schema';
 
 export class CreatePostDTO {
   @IsNotEmpty()
+  @ApiProperty()
   title: string;
+
   @IsNotEmpty()
+  @ApiProperty()
   content: string;
   author: User;
 }
