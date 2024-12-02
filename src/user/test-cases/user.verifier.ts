7fcab487a5c5e6e87c4199bec9e3f1929b5ae239
fuzzy <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2024-12-02 16:05:05 +0600

    seperate input output

---
+++ src/user/test-cases/user.verifier.ts
@@ -0,0 +1,10 @@
+// TestCases.ts
+export class TestVerifier {
+  static createdUser = {
+    _id: '674d7ad5fd6df0f1c9020fab',
+    name: 'User',
+    skills: ['A'],
+    experience: 1,
+    _v0: 0,
+  };
+}
