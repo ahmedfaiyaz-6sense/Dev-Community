5c91118417fbad2118a31c244efcf5b5e5de7a30
fuzzy <188864756+ahmedfaiyaz-6sense@users.noreply.github.com>
2024-12-02 17:27:38 +0600

    update user test

--- src/user/test-cases/user.verifier.ts
+++ src/user/test-cases/user.verifier.ts
@@ -8,4 +8,12 @@ export class TestVerifier {
     _v0: 0,
   };
   static loggedInUser = { access_token: 'mocked_token' };
+
+  static updatedUser = {
+    _id: '674d7ad5fd6df0f1c9020fax',
+    username: 'User-1',
+    skills: ['C'],
+    experience: 10,
+    _v0: 0,
+  };
 }
